37cefbf676270df260fb24ceaa01cd94b2429fb1
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-11 11:02:46 +0000

    Initial commit

---
+++ src/main/java/com/codeclan/example/EmployeesLab/EmployeesLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.EmployeesLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/EmployeesLab/EmployeesLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.EmployeesLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
