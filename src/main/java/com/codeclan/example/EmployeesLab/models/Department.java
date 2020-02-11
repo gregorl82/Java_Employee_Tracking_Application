bbf1be8708a356374a17018920cc9da491bfc577
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-11 11:10:48 +0000

    Adds Department model

---
+++ src/main/java/com/codeclan/example/EmployeesLab/models/Department.java
@@ -0,0 +1,55 @@
+package com.codeclan.example.EmployeesLab.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "employees")
+    private List<Employee> employees;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Department(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public Department() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
+}
